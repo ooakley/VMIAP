08a94e9a54b7d67c3f0a40f966178d44178d9e93
Omar El Oakley <49024315+ooakley@users.noreply.github.com>
2020-01-17 16:05:44 +0000

    Running tests on pybedtools

--- code_scrapbook.py
+++ code_scrapbook.py
@@ -1,6 +1,17 @@
 """
 A place to put odd bits of code under construction/for use in notebooks.
+
+Example:
+python code_scrapbook.py
 """
 
 import numpy as np
 import seaborn as sns
+import pybedtools
+
+a = pybedtools.example_bedtool('a.bed')
+b = pybedtools.example_bedtool('b.bed')
+
+iap_smush = 
+
+print(a.intersect(b))
